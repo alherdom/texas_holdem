73c3563472c009b859cbf784b515c93b2cbabde6
alherdom <alejandroh1@live.com>
2023-06-12 02:07:12 +0100

    Sync

--- src/cards.py
+++ src/cards.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+
 class Card:
     SYMBOLS = ('A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K')
     A_VALUE = 1
@@ -43,8 +44,7 @@ class Hand:
     
     def __init__(self, cards: list[Card]):
         self.hand = cards
-        self.cat_rank: str | tuple[str]
-    
+
     def __getitem__(self, index: int) -> Card:
         return self.hand[index]
     
@@ -61,25 +61,14 @@ class Hand:
         return card in self.hand
     
     def __gt__(self, other) -> bool:
-        if self.cat < other.cat:
-            return False
-        if self.cat == other.cat:
-            for card1, card2 in zip(self.hand, other.hand):
-                if card1 < card2:
-                    return False
-        return True
-    
-    def __lt__(self, other) -> bool:
-        for card1, card2 in zip(self.hand, other.hand):
-            if card1 > card2:
-                return False
-        return True
+        if self.cat > other.cat:
+            return True
+        if self.cat == other.cat and self.cat_rank > other.cat_rank:
+            return True
+        return False
         
     def __eq__(self, other) -> bool:
-        for card1, card2 in zip(self.hand, other.hand):
-            if card1 != card2:
-                return False
-        return True
+        return self.cat == other.cat and self.cat_rank == other.cat_rank
     
     def is_straight_flush(self, values: list[int], len_set_values: int, len_set_suits: int) -> bool:
         return len_set_suits == 1 and len_set_values == 5 and values[-1] - values[0] == 4
@@ -129,7 +118,21 @@ class Hand:
         if self.is_one_pair(len_set_values):
             return Hand.ONE_PAIR
         return Hand.HIGH_CARD
-        
+    
+    @property
+    def cat_rank(self) -> str | tuple[str]:
+        values = [card.value for card in self.hand]
+        values = sorted(values, key=lambda v: values.count(v), reverse=True)
+        item1 = Card.SYMBOLS[values[0] - 1]
+        if self.cat in [Hand.ONE_PAIR, Hand.THREE_OF_A_KIND, Hand.FOUR_OF_A_KIND]:
+            return item1
+        if self.cat == Hand.TWO_PAIR:
+            item2 = Card.SYMBOLS[values[2] - 1]
+            return tuple(sorted((item1, item2), reverse = True))
+        if self.cat == Hand.FULL_HOUSE:
+            item2 = Card.SYMBOLS[values[3] - 1]
+            return tuple(item1, item2)
+        return Card.SYMBOLS[max(values) - 1]
         
 class HandIterator:
     def __init__(self, hand: Hand):
